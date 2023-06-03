4681994cd69b3ce64266d5f03deb87e90728bed4
Ömer Alakuş <alakusomer@gmail.com>
2023-06-03 13:40:27 +0300

    Bird Script Update

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -5,18 +5,63 @@ using UnityEngine;
 public class BirdScript : MonoBehaviour
 {
 
-    
+    // 2D RigidBody Component Reference
+    public Rigidbody2D birdRig;
+
+    // Birds's Gravity Scale 
+    public float gravityScale = 1;
+
+    // Bird's Vertical Movement Scale 
+    public float upperMovement = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.name = "Retarded Bird ^^ ";
-        
-        
+        gameObject.name = "Bird Character";
+
+
+        // Update Gravity Scale on BeginPlay 
+        setGravityScale();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        // Update Bird Vertical Movement on Each Draw Call { Tick }
+
+        moveBird();
+
+    }
+
+    void moveBird()
+    {
+
+
+        // Update the Bird Vertical Velocity by Input Key Down
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            birdRig.velocity = Vector2.up * upperMovement;
+        }
+
+
+    }
+
+    void setGravityScale()
+    {
+
+        // Set Bird's Gravity Scale
+        birdRig.gravityScale = gravityScale;
+
+    }
+
+    void setLocalPosition()
+    {
+
+        // Set the bird's transform to the bird's transform
+        transform.position = transform.localPosition;
     }
 }
