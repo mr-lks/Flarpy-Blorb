8b501800b058c876398950d378cb39b62d6f034d
Ömer Alakuş <alakusomer@gmail.com>
2023-06-03 12:20:35 +0300

    main

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.name = "Retarded Bird ^^ ";
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
